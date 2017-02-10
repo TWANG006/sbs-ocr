09e5883d86b5059c57664c5ba989cca6d4dbcb3a
TWANG006 <twang006@e.ntu.edu.sg>
2017-02-10 10:47:18 +0800

    Test passed, doing the demo

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,16 +1,48 @@
 #include <iostream>
-#include <vector>
+#include <memory>
+
+#include <allheaders.h> // leptonica main header for image io
+#include <baseapi.h> // tesseract main header
 #include "gtest\gtest.h"
 
-
-using namespace std;
-
-int main(int argc, char** argv)
+int main(int argc, char *argv[])
 {
-	/* Need to be revised*/
+    if (argc == 1)
+        return 1;
+
+    tesseract::TessBaseAPI tess;
+
+    if (tess.Init("./tessdata", "eng"))
+    {
+        std::cout << "OCRTesseract: Could not initialize tesseract." << std::endl;
+        return 1;
+    }
+
+    // setup
+    tess.SetPageSegMode(tesseract::PageSegMode::PSM_AUTO);
+    tess.SetVariable("save_best_choices", "T");
+
+    // read image
+    auto pixs = pixRead(argv[1]);
+    if (!pixs)
+    {
+        std::cout << "Cannot open input file: " << argv[1] << std::endl;
+        return 1;
+    }
+
+    // recognize
+    tess.SetImage(pixs);
+    tess.Recognize(0);
+
+    // get result and delete[] returned char* string
+    std::cout << std::unique_ptr<char[]>(tess.GetUTF8Text()).get() << std::endl;
+
+    // cleanup
+    tess.Clear();
+    pixDestroy(&pixs);
 
 	::testing::InitGoogleTest(&argc, argv);
 	RUN_ALL_TESTS();
 
-	return 0;
+    return 0;
 }
